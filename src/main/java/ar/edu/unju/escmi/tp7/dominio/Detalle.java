1c69167c65e6521d178b2727762c8474fc78593c
lulipereira <lulipereira2006@gmail.com>
2025-11-02 23:06:17 -0300

    Stock , Factura y Detalle [ Luisana Pereira ]

--- src/main/java/ar/edu/unju/escmi/tp7/dominio/Detalle.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dominio/Detalle.java
@@ -1,52 +1,26 @@
 package ar.edu.unju.escmi.tp7.dominio;
 
 public class Detalle {
-
-	private int cantidad;
+    private int cantidad;
     private double importe;
     private Producto producto;
 
-    public Detalle() {
-
-    }
-
-    public Detalle(int cantidad, double importe, Producto producto) {
+    public Detalle(int cantidad, Producto producto) {
         this.cantidad = cantidad;
-        this.importe = importe;
         this.producto = producto;
-        calcularImporte();
+        this.importe = calcularImporte();
     }
 
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public double getImporte() {
-        return importe;
-    }
-
-    public void setImporte(double importe) {
-        this.importe = importe;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
-    private void calcularImporte() {
-        this.setImporte(this.cantidad * this.producto.getPrecioUnitario());
+    public double calcularImporte() {
+        return producto.getPrecioUnitario() * cantidad;
     }
 
     @Override
     public String toString() {
-        return "PRODUCTO: " + producto + "\nCANTIDAD: " + cantidad + " | IMPORTE: " + importe + "\n";
+        return "Detalle{" +
+                "cantidad=" + cantidad +
+                ", importe=" + importe +
+                ", producto=" + producto +
+                '}';
     }
 }
--- src/main/java/ar/edu/unju/escmi/tp7/dominio/Stock.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dominio/Stock.java
@@ -2,59 +2,25 @@ package ar.edu.unju.escmi.tp7.dominio;
 
 public class Stock {
     private int cantidad;
-    private Producto producto;
 
-    public Stock() {
-    }
-
-    public Stock(int cantidad, Producto producto) {
-        this.cantidad = cantidad;
-        this.producto = producto;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
+    public Stock(int cantidad) {
         this.cantidad = cantidad;
     }
 
-    public Producto getProducto() {
-        return producto;
+    public boolean validarStockDisponible() {
+        return cantidad > 0;
     }
 
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
-    /**
-     * Verifica si hay stock suficiente para una cantidad solicitada.
-     */
-    public boolean hayStockDisponible(int cantidadSolicitada) {
-        return cantidadSolicitada <= cantidad;
-    }
-
-    /**
-     * Descuenta stock si hay suficiente. Devuelve true si se pudo descontar.
-     */
-    public boolean descontarStock(int cantidadSolicitada) {
-        if (hayStockDisponible(cantidadSolicitada)) {
-            cantidad -= cantidadSolicitada;
-            return true;
+    public void actualizarStock() {
+        if (cantidad > 0) {
+            cantidad--;
         }
-        return false;
     }
 
-@Override
-public String toString() {
-    if (producto == null) {
-        return "Producto no asignado | Stock disponible: " + cantidad;
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "cantidad=" + cantidad +
+                '}';
     }
-
-    return "Producto: " + producto.getDescripcion()
-         + " | Origen: " + producto.getOrigenFabricacion()
-         + " | Precio: $" + producto.getPrecioUnitario()
-         + " | Stock disponible: " + cantidad;
-}
 }
